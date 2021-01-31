310c27e4f0aa32ee50b32a158d73b9fb952b2ffb
Jesse Rodrigues Bastos <jesse.r.bastos@gmail.com>
2021-01-31 18:39:35 -0300

    MY08-MY11 Organizando com EJS-includes, 1º INSERT (Reserva)..

--- routes/index.js
+++ routes/index.js
@@ -1,23 +1,18 @@
 var conn = require('./../inc/db');
+var menus = require('./../inc/menus');
+var reservations = require('./../inc/reservations');
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
-  conn.query(`
-    SELECT * FROM tb_menus ORDER BY title
-  `, (err, result)=>{
-
-    if (err) {
-
-      console.log(err);
-
-    }
+  menus.getMenus().then(results =>{
 
     res.render('index', {
       title: 'Restaurante Saboroso!',
-      menus: result
+      menus: results,
+      isHome: true
     });
 
   });
@@ -46,21 +41,52 @@ router.get('/services', function(req, res, next){
 
 router.get('/menus', function(req, res, next){
 
-  res.render('menus', {
-      title: 'Menus - Restaurante Saboroso!',
-      background: 'images/img_bg_1.jpg',
-      h1: 'Saboreie nosso menu!'
-    });
+  menus.getMenus().then(results=>{
+
+    res.render('menus', {
+        title: 'Menus - Restaurante Saboroso!',
+        background: 'images/img_bg_1.jpg',
+        h1: 'Saboreie nosso menu!',
+        menus: results
+      });
+
+  });
 
 });
 
 router.get('/reservations', function(req, res, next){
 
-  res.render('reservations', {
-      title: 'Reservas - Restaurante Saboroso!',
-      background: 'images/img_bg_2.jpg',
-      h1: 'Reserve uma Mesa!'
+  reservations.render(req, res, "", "");
+
+});
+
+router.post('/reservations', function(req, res, next){
+
+  if (!req.body.name) {
+      reservations.render(req, res, "Digite seu Nome");
+  } else if (!req.body.email) {
+      reservations.render(req, res, "informe um E-mail para contato!");
+  } else if (!req.body.people) {
+      reservations.render(req, res, "informe o número de pessoas!");
+  } else if (!req.body.date) {
+      reservations.render(req, res, "informe a Data da Reserva!");
+  } else if (!req.body.time) {
+      reservations.render(req, res, "informe o horário da Reserva!");
+  } else {
+
+    reservations.save(req.body).then(results=>{
+
+      req.body = {};
+
+      reservations.render(req, res, null ,"Reserva realizada com sucesso!");
+
+    }).catch(err=>{
+
+      reservations.render(req, res, err.message);
+
     });
+
+  }
 
 });
 
